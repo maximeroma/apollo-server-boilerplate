510dbca8972a08c4021f792d1c6958c82b8adf1d
maxime roma <roma.maxime1994@gmail.com>
2018-09-01 21:42:59 +0200

    setting up postgres sequelize

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from 'express'
+import 'dotenv/config'
 import cors from 'cors'
 import { ApolloServer } from 'apollo-server-express'
 import schemas from './schemas'
 import resolvers from './resolvers'
-import models from './models'
+import models, { sequelize } from './models'
 
 const app = express()
-
+console.log('hello1', process.env.DATABASE)
 app.use(cors())
 
 const server = new ApolloServer({
@@ -14,10 +15,22 @@ const server = new ApolloServer({
   resolvers,
   context: {
     models,
-    me: models.users[1]
+    me: models.users
   }
 })
 
 server.applyMiddleware({ app, path: '/graphql' })
 
-app.listen({ port: 8000 }, () => console.log('Apollo server on port 8000'))
+sequelize
+  .sync()
+  //   // .sync({ force: true })
+  //   // .authenticate()
+  //   // .then(() => {
+  //   //   console.log('Connection has been established successfully.')
+  //   // })
+  //   // .catch(err => {
+  //   //   console.error('Unable to connect to the database:', err)
+  //   // })
+  .then(async () => {
+    app.listen({ port: 8000 }, () => console.log('Apollo server on port 8000'))
+  })
--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,26 @@
-let users = {
-  1: { id: '1', firstname: 'Maxime', lastname: 'Roma', messageIds: [1] },
-  2: { id: '2', firstname: 'Dave', lastname: 'Davids', messageIds: [2] }
-}
+import Sequelize from 'sequelize'
 
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '2'
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2'
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'postgres'
   }
+)
+
+const models = {
+  User: sequelize.import('./user'),
+  Message: sequelize.import('./message')
 }
 
-export default { messages, users }
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models)
+  }
+})
+
+export { sequelize }
+
+export default models
